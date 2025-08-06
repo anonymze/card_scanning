c1ee8e7717497c3c6b975eceaba198b26ebeeff1
anonymze <anodevfr@gmail.com>
2025-08-07 01:16:37 +0200

    remove: radon ai

--- metro.config.cjs
+++
@@ -1,7 +0,0 @@
-const { withNativeWind } = require("nativewind/metro");
-const { getSentryExpoConfig } = require("@sentry/react-native/metro");
-
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getSentryExpoConfig(__dirname);
-
-module.exports = withNativeWind(config, { input: "./src/styles/global.css" });
