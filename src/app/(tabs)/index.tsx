1b77708d7538ee7dede804653ad661c5cd8de6bc
anonymze <anodevfr@gmail.com>
2025-08-08 01:57:20 +0200

    add: loader

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,48 @@
-import { CameraNoPermissions, CameraUnavailable } from '@/components/camera';
-import { StyleSheet } from 'react-native';
+import {
+  CameraNoPermissions,
+  CameraUnavailable,
+  LayoutCamera,
+} from '@/components/camera';
+import { ScanIcon } from '@/components/icons';
+import { ButtonPrimary } from '@/components/ui/buttons';
+import { useLoaderGlobal } from '@/lib/loader-store';
+import React from 'react';
+import { Text, View } from 'react-native';
 import {
   Camera,
   useCameraDevice,
   useCameraPermission,
 } from 'react-native-vision-camera';
+import config from 'tailwind.config';
 
 export default function Page() {
-  const device = useCameraDevice('front');
-  const { hasPermission } = useCameraPermission();
+  const { start, stop, loading } = useLoaderGlobal()
+  const device = useCameraDevice('back');
+  const { hasPermission, requestPermission } = useCameraPermission();
 
-  if (!hasPermission) return <CameraNoPermissions />;
-  if (device == null) return  <CameraUnavailable />;
+  if (!hasPermission) {
+    requestPermission();
+    return <CameraNoPermissions />;
+  }
+  if (device == null) return <CameraUnavailable />;
 
   return (
-    <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+    <LayoutCamera>
+      <View className="w-full flex-1 overflow-hidden rounded-3xl">
+        <Camera
+          style={{
+            position: 'absolute',
+            inset: 0,
+            borderRadius: 30,
+          }}
+          device={device}
+          isActive={loading}
+        />
+        <ButtonPrimary action={() => {
+          if (loading) return stop();
+          start();
+        }} title="Scanner" icon={<ScanIcon color={config.theme.extend.colors.foreground} />} className='mt-auto rounded-none' />
+      </View>
+    </LayoutCamera>
   );
 }
