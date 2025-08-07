305ea0235079ac92821bfb957004915bb78a995d
anonymze <anodevfr@gmail.com>
2025-08-07 18:07:35 +0200

    add: loader

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,6 +1,18 @@
+import { useLoaderGlobal } from "@/lib/loader-store";
+import { useFocusEffect } from "expo-router";
+import React from "react";
 import { Text, View } from "react-native";
 
 export default function Page() {
+  const { start } = useLoaderGlobal();
+
+  useFocusEffect(
+    React.useCallback(() => {
+      console.log("Decks tab focused");
+      start();
+    }, [start])
+  );
+
   return (
     <View className="flex-1 bg-red-200">
       <Text>Okiddddd</Text>
