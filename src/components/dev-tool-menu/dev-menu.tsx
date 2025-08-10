799939eafb8f8360c62a06b7e9009961fdd9b9f1
anonymze <anodevfr@gmail.com>
2025-08-11 01:20:35 +0200

    add: dark mode

--- src/components/dev-tool-menu/dev-menu.tsx
+++ src/components/dev-tool-menu/dev-menu.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from '@/styles/theme';
 import * as DevClient from 'expo-dev-client';
 import { Text, TouchableOpacity, View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 const DevMenu = () => {
   const { theme, toggleTheme } = useTheme();
@@ -13,16 +14,24 @@ const DevMenu = () => {
     },
   ]);
 
+  const gesture = Gesture.Tap().runOnJS(true).onEnd((ev) => {
+    console.log(ev.x, ev.y);
+    toggleTheme()
+    // DevClient.openMenu();
+  });
+
   return (
-    <View style={{ position: 'absolute', top: 50, right: 20, zIndex: 9999 }}>
-      <TouchableOpacity
-        hitSlop={8}
-        onPress={() => DevClient.openMenu()}
-        style={{ backgroundColor: '#ff6b6b', padding: 10, borderRadius: 5 }}
-      >
-        <Text style={{ color: 'white', fontWeight: 'bold' }}>🛠️ Dev</Text>
-      </TouchableOpacity>
-    </View>
+    <GestureDetector gesture={gesture}>
+      <View style={{ position: 'absolute', top: 50, right: 20, zIndex: 9999 }}>
+        <TouchableOpacity
+          hitSlop={8}
+          // onPress={() => DevClient.openMenu()}
+          style={{ backgroundColor: '#ff6b6b', padding: 10, borderRadius: 5 }}
+        >
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>🛠️ Dev</Text>
+        </TouchableOpacity>
+      </View>
+    </GestureDetector>
   );
 };
 
